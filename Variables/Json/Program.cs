f2260ba7ad0d4683198b511e22a62b224dcc060b
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2022-08-27 16:34:40 -0500

    Video 19 y 20

---
+++ Variables/Json/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+namespace Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beer objBeer = new Beer(){ Name = "Pilser",Brand="CN" };
+            //creacion de un json
+            string json = JsonSerializer.Serialize(objBeer);
+            Console.WriteLine(json);
+            Beer objBerr2 = JsonSerializer.Deserialize<Beer>(json);
+            Console.WriteLine(objBerr2);
+
+        }
+    }
+    public class Beer
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+    }
+}
