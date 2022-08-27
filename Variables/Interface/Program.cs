9ad6816587adeb6e5d911e285247993246531a95
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2022-08-27 01:42:14 -0500

    INTERFACES FIN

--- Variables/Interface/Program.cs
+++ Variables/Interface/Program.cs
@@ -9,4 +9,25 @@ namespace Interface
             Console.WriteLine("Hello World!");
         }
     }
+    public interface IAnimal
+    {
+        public string Name { get; set; }
+    }
+    public interface IFish
+    {
+        public int Speed { get; set; }
+        public string Swim();
+    }
+
+    // usando las intefaces
+    public class Shark : IAnimal, IFish
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public string Swim()
+        {
+            return $"{Name} Nada {Speed}";
+        }
+    }
+
 }
