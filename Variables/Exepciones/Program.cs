c1825ece7efcce7a95c707b99ee1a1380b9ee550
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2022-09-06 01:45:46 -0500

    Inicio Exepciones

---
+++ Variables/Exepciones/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
