8fe3e285e18544b98d1e479b37d8dcc09f6e8604
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2022-09-06 02:23:24 -0500

    Inicion exepciones

--- Variables/Exepciones/Program.cs
+++ Variables/Exepciones/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-
+using System.IO;
 namespace Exepciones
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try 
+            {
+                throw new Exception("Ocurrio algo raro");
+                string content = File.ReadAllText(@"C:\xampp\htdocs\Curso_C-\archivos_prueba.txt");
+                Console.WriteLine(content);
+            }
+            catch(FieldAccessException e)
+            {
+                Console.WriteLine($" ERROR ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //cerrar el archivo
+            finally
+            {
+                Console.WriteLine("CERRAMOS EL ARCHIVO Y FINALIZADO PASE LO Q PASE ");
+            }
         }
     }
 }
---
+++ Variables/ExepcionesPersonalizadas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExepcionesPersonalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Beer objBeer = new Beer();
+                objBeer.Brand = "Fuller";
+                Console.WriteLine(objBeer);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+    class Beer
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+
+        public override string ToString()
+        {
+            if (Name == null || Brand == null)
+                throw new Exception();
+            return $"Cerveza: {Name },Brand: {Brand}";
+        }
+
+
+    }
+}
