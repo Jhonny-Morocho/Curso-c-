383ba5c20e40c8b4d8bb0902fbf9133816d9d5cd
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2022-09-07 03:40:43 -0500

    CONEXION_BD

---
+++ Variables/CONEXION_BD/DB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONEXION_BD
+{
+    class DB
+    {
+        private string _strConexion;
+        protected SqlConnection _connection;
+
+        public DB(string strServe,string db)
+        {
+            _strConexion = $"Data Source={strServe}; Initial Catalog={db};Integrated Security=true";
+        }
+
+        public void Connect()
+        {
+            _connection = new SqlConnection(_strConexion);
+            _connection.Open();
+            Console.WriteLine("CONEXION EXITOSA");
+        }
+        public void Close()
+        {
+            if (_connection!=null && _connection.State==System.Data.ConnectionState.Open)
+            {
+                _connection.Close();
+
+            }
+
+        }
+    }
+}
---
+++ Variables/CONEXION_BD/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace CONEXION_BD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LLAMAMOS A LA BASE DE DATOS
+
+            try
+            {
+                DB dbConexion = new DB(@"DESKTOP-CCMB5TR\SQLEXPRESS","curso_c_dev");
+                dbConexion.Connect();
+                dbConexion.Close();
+
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine($"ERROR AL CONECTARSE {e.Message}");
+            }
+        }
+    }
+}
